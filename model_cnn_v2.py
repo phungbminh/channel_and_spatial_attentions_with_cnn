9774cc53cc0f695411468209df41cb828ff3759d
Bui Minh Phung <phungbminh@gmail.com>
2024-12-30 01:40:39 +0700

    update use atteion afer 123

--- model_cnn_v2.py
+++ model_cnn_v2.py
@@ -92,6 +92,7 @@ def ResNet(model_name="ResNet50", input_shape=(48, 48, 3), a_output='softmax', p
     x = layers.Conv2D(64, 7, strides=2, name='Conv1')(x)
     x = layers.BatchNormalization(axis=batch_axis, epsilon=1.001e-5, name='Conv1_BN')(x)
     x = layers.Activation('relu', name='Conv1_relu')(x)
+
     #
     # x = layers.ZeroPadding2D(padding=((1, 1), (1, 1)), name='MaxPool2D_1_Pad')(x)
     # x = layers.MaxPooling2D(3, strides=2, name='MaxPool2D_1')(x)
@@ -104,7 +105,7 @@ def ResNet(model_name="ResNet50", input_shape=(48, 48, 3), a_output='softmax', p
     print('Stage 3: Conv4_x{}'.format(num_blocks[2]))
     x = stage(x, [256, 256, 1024], num_blocks[2], name='Conv4_x{}'.format(num_blocks[2]), attention=attention)
     print('Stage 4: Conv5_x{}'.format(num_blocks[3]))
-    x = stage(x, [512, 512, 2048], num_blocks[3], name='Conv5_x{}'.format(num_blocks[2]), attention=attention)
+    x = stage(x, [512, 512, 2048], num_blocks[3], name='Conv5_x{}'.format(num_blocks[2]), attention=None)
 
     # Output
     if pooling == 'avg':
--- train.py
+++ train.py
@@ -108,11 +108,11 @@ def main():
     if args.model == 'resnet18':
         # model = resnet18(input_shape=(args.image_size, args.image_size, args.image_channels), num_classes=classes,
         #               attention_type=args.attention_option)
-        # model = ResNet(model_name="ResNet18", input_shape=(args.image_size, args.image_size, args.image_channels),
-        #                attention=args.attention_option,  pooling="avg")
+        model = ResNet(model_name="ResNet18", input_shape=(args.image_size, args.image_size, args.image_channels),
+                       attention=args.attention_option,  pooling="avg")
 
-        weight_decay = 1e-4
-        model = ResNet18(input_shape=(args.image_size, args.image_size, args.image_channels), classes=classes, weight_decay=weight_decay, attention=args.attention_option)
+        # weight_decay = 1e-4
+        # model = ResNet18(input_shape=(args.image_size, args.image_size, args.image_channels), classes=classes, weight_decay=weight_decay, attention=args.attention_option)
     if args.model == 'vgg16':
         model = VGG16(input_shape=(args.image_size, args.image_size, args.image_channels), num_classes=classes,
                       attention_type=args.attention_option)
